27b3886277a0970a37a264a4c27f2e095d73d3e5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-12-31 01:57:19 +0100

    Feat: Completed setup of Vue 3 and Tailwind CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import { createApp } from "vue";
+
+import TestComponent from "./components/TestComponent.vue";
+import NewComponent from "./components/NewComponent.vue";
+
+createApp({
+    components: { TestComponent, NewComponent },
+}).mount("#app");
