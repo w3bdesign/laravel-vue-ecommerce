99eed0955f727330c84b7801c0dcc69711395e25
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-14 00:16:41 +0100

    Add test for purchasing a product

---
+++ tests/Browser/BuyProductTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class BuyProductTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+}
