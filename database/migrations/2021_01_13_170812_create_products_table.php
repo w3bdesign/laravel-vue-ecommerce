cfd738189432d8b6bfb02be4467815e899fea9d4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-13 21:21:48 +0100

    Add product fields

--- database/migrations/2021_01_13_170812_create_products_table.php
+++ database/migrations/2021_01_13_170812_create_products_table.php
@@ -15,10 +15,13 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->integer('price')->default(0);
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
