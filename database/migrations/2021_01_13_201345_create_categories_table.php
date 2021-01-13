889b76fa3123f6032c1e3091510b2fe7c82c6bf3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-13 21:24:02 +0100

    Setup Category model fields

--- database/migrations/2021_01_13_201345_create_categories_table.php
+++ database/migrations/2021_01_13_201345_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
