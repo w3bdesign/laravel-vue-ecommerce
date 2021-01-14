cabd489d24f30f1972046a67758a2235e67ba03d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-14 04:47:20 +0100

    Add relations for Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,14 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
 }
