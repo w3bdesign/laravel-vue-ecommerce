772d5f88bed2155cefb440fcb6e6e6ed83c4918c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-14 04:44:41 +0100

    Add guarded property to all models
    You will need to specify either a fillable or guarded property on your model class. These properties are required because all Eloquent models are protected against mass assignment vulnerabilities by default.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
