be48911ee03d29c2783a34d5c4054b5dd149c715
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-15 07:45:00 +0100

    Fix browser test

--- tests/Browser/BuyProductTest.php
+++ tests/Browser/BuyProductTest.php
@@ -17,7 +17,7 @@ class BuyProductTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('http://127.0.0.1:8000')
-                    ->assertSee('Laravel Ecommerce');
+                    ->assertSee('Laravel Vue 3 Ecommerce');
         });
     }
 }
