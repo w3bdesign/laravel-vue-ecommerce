52034644852a669039bd625591302e64ab88114c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-15 04:54:49 +0100

    Add code for product controller

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return Product::with(['categories' => function ($query) {
+            $query->select('id', 'name');
+        }])
+            ->get();
+    }
+
+    public function show(Product $product)
+    {
+        $product->load('categories:id,name');
+
+        return $product;
+    }
 }
