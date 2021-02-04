ff3ca4efa81a2618956003f74a0b819e283477d5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-02-04 02:49:44 +0100

    Start work on checkout page

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,12 +6,11 @@ import store from './store/index';
 import Layout from './components/Layout/Layout.vue';
 import FooterContent from './components/Layout/Footer/FooterContent.vue';
 import Cart from './components/Layout/Header/Cart.vue';
-import CartContents from './components/Cart/CartContents.vue';
+import Categories from './components/Categories/Categories.vue';
 
 import Index from './pages/Home/Index.vue';
-
 import Products from './pages/Products/Products.vue';
-import Categories from './components/Categories/Categories.vue';
+import Checkout from './pages/Checkout/Checkout.vue';
 
 require('./bootstrap');
 
@@ -19,7 +18,7 @@ const routes = [
   { path: '/', component: Index },
   { path: '/products', component: Products },
   { path: '/categories', component: Categories },
-  { path: '/cart', component: CartContents },
+  { path: '/checkout', component: Checkout },
 ];
 
 const router = createRouter({
@@ -29,7 +28,13 @@ const router = createRouter({
 
 createApp({
   components: {
-    FooterContent, Cart, Layout, Index, Products, Categories, CartContents,
+    FooterContent,
+    Cart,
+    Layout,
+    Index,
+    Products,
+    Categories,
+    Checkout,
   },
 })
   .use(store)
---
+++ resources/js/pages/Checkout/Checkout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    This is the checkout page We will list products and have a checkout here
+    <br>
+    Cart: {{ $store.state.cart }}
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
