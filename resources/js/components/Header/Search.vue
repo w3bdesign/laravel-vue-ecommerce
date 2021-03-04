db7663f9ba0e2037fc3267ed20b0c60636f69743
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2021-03-04 07:13:29 +0100

    Replace Algolia article
    Algolia search doesn't work on Vue 3

--- resources/js/components/Header/Search.vue
+++ resources/js/components/Header/Search.vue
@@ -1,1 +1,1 @@
-// https://www.algolia.com/doc/framework-integration/laravel/tutorials/getting-started-with-laravel-scout-vuejs/?client=php
+// https://tighten.co/blog/full-text-search-with-meilisearch-and-scout/
