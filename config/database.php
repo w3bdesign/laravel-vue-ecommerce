4141f8ec1b6b2b7aa5e096a1f0c3f825f10796f0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-11 21:05:38 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'sqlite_testing' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database_testing.sqlite')),
+            'database' => env('DB_DATABASE', database_path('testing.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
