ced87b3a31742b63906886e45740685e94f6c427
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-11 03:16:34 +0200

    Implementer Vue 3 med Laravel 9

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import { createApp } from "vue";
+import HelloWorld from "./components/Welcome";
+
+const app = createApp({});
+
+app.component("hello-world", HelloWorld);
+
+app.mount("#app");
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+<script>
+export default {
+    setup: () => ({
+        title: "How To Install Vue 3 in Laravel 9 From Scratch",
+    }),
+};
+</script>
