4751fb659478007278ba3add35f8c9f27866bf00
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-11 03:01:31 +0200

    Oppsett Vue 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js("resources/js/app.js", "public/js")
+    .vue()
+    .postCss("resources/css/app.css", "public/css", [
         //
     ]);
