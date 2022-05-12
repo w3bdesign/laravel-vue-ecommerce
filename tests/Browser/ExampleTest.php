850a298ce15d4d431a76aa8cf1f9dfa4f2266bf0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-12 05:04:40 +0200

    Update ExampleTest.php

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -18,7 +18,7 @@ class ExampleTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
 
             $browser->visit('/')
-                ->assertSee("Laravel");
+                ->assertSee("Vue");
             //->assertPathIs('/');
 
             // ->assertSee('Blade test dusk');
