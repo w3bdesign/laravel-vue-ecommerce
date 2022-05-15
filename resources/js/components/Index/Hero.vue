1a9ffa7b321678cb1e02fba2be75e6cd5212dace
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-15 23:42:13 +0200

    ShowAllProducts

--- resources/js/components/Index/Hero.vue
+++ resources/js/components/Index/Hero.vue
@@ -2,7 +2,7 @@
     <div id="hero" class="flex w-full mx-auto mt-16 bg-right bg-cover md:pt-0 md:items-center">
         <div class="flex flex-col items-start justify-center w-full px-6 tracking-wide lg:w-1/2">
             <span class="p-4 my-4 text-2xl text-black bg-white rounded-lg">
-                MacBook Air 14 In Store now!
+                MacBook Air 14 In Store now!11
             </span>
         </div>
     </div>
