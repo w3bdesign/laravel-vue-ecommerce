fde7f5cd49b3ca0d2c5c0f94962b29485325a9bf
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-06-30 01:36:49 +0200

    Try to make Favicon work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,9 @@
 
     <title>Laravel Vue</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
+
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
