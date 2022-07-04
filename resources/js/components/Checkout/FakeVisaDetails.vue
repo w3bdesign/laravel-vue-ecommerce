0bd5ef5f6b516733dcdcd7e26e7280abc10ddcf3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-07-04 02:30:17 +0200

    Responsive table

--- resources/js/components/Checkout/FakeVisaDetails.vue
+++ resources/js/components/Checkout/FakeVisaDetails.vue
@@ -37,6 +37,89 @@
                 </tr>
             </tbody>
         </table>
+
+
+
+        <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+			<thead class="text-white">
+				<tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+					<th class="p-3 text-left">Name</th>
+					<th class="p-3 text-left">Email</th>
+					<th class="p-3 text-left" width="110px">Actions</th>
+				</tr>
+				<tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+					<th class="p-3 text-left">Name</th>
+					<th class="p-3 text-left">Email</th>
+					<th class="p-3 text-left" width="110px">Actions</th>
+				</tr>
+                <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                  <th class="p-3 text-left">Name</th>
+                  <th class="p-3 text-left">Email</th>
+                  <th class="p-3 text-left" width="110px">Actions</th>
+              </tr>
+                <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                  <th class="p-3 text-left">Name</th>
+                  <th class="p-3 text-left">Email</th>
+                  <th class="p-3 text-left" width="110px">Actions</th>
+              </tr>
+			</thead>
+			<tbody class="flex-1 sm:flex-none">
+				<tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+					<td class="border-grey-light border hover:bg-gray-100 p-3">John Covv</td>
+					<td class="border-grey-light border hover:bg-gray-100 p-3 truncate">contato@johncovv.com</td>
+					<td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+				</tr>
+				<tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+					<td class="border-grey-light border hover:bg-gray-100 p-3">Michael Jackson</td>
+					<td class="border-grey-light border hover:bg-gray-100 p-3 truncate">m_jackson@mail.com</td>
+					<td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+				</tr>
+                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">Julia</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">julia@mail.com</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+                </tr>
+                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                  <td class="border-grey-light border hover:bg-gray-100 p-3">Martin Madrazo</td>
+                  <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">martin.madrazo@mail.com</td>
+                  <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+                </tr>
+			</tbody>
+		</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
 </template>
 
@@ -47,3 +130,23 @@ import { useCart } from "../../store/useCart";
 
 const { getCustomerDetails } = storeToRefs(useCart());
 </script>
+
+<style>
+@media (min-width: 640px) {
+    table {
+      display: inline-table !important;
+    }
+
+    thead tr:not(:first-child) {
+      display: none;
+    }
+  }
+
+  td:not(:last-child) {
+    border-bottom: 0;
+  }
+
+  th:not(:last-child) {
+    border-bottom: 2px solid rgba(0, 0, 0, .1);
+  }
+</style>
