d411930af91cf58441a111842df4df9b145eef53
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-07-07 02:43:49 +0200

    Code cleanup

--- resources/js/hooks/useGetProducts.js
+++ resources/js/hooks/useGetProducts.js
@@ -2,18 +2,15 @@ import axios from "axios";
 
 async function useGetProducts() {
     let products;
-    let loading;
 
     try {
-        loading = true;
         const response = await axios.get("/api/products");
         products = response.data;
-        loading = false;
     } catch (error) {
         this.error = error;
     }
 
-    return { products, loading };
+    return { products };
 }
 
 export default useGetProducts;
