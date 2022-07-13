5689a90fceabb689c43bdb3ba92b76530b517e2f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-07-13 03:41:12 +0200

    Test all table props

--- tests/Vue/Table/table.spec.js
+++ tests/Vue/Table/table.spec.js
@@ -1,13 +1,47 @@
-import { render } from "@testing-library/vue";
+import { render, screen } from "@testing-library/vue";
 import BaseTable from "../../../resources/js/components/base/BaseTable.vue";
 
 import "@testing-library/jest-dom";
 
-test("Verify that the tableTitle prop works", async () => {
-    const { getByText } = render(BaseTable, {
-        props: { tableTitle: "Table title test" },
+describe("BaseTable", () => {
+    beforeEach(() => {
+        render(BaseTable, {
+            props: {
+                tableTitle: "card details for testing",
+                tableHeaders: [
+                    "Name",
+                    "Address",
+                    "Zipcode",
+                    "City",
+                    "State",
+                    "Email",
+                ],
+                tableData: [
+                    "firstName",
+                    "address",
+                    "zipcode",
+                    "city",
+                    "state",
+                    "email",
+                ],
+            },
+        });
     });
 
-    const tableTitle = getByText("Table title test");
-    expect(tableTitle).toBeInTheDocument();
+    test("Verify that the tableTitle prop works", async () => {
+        const tableTitle = screen.getByRole("heading", {
+            name: /card details for testing/i,
+        });
+        expect(tableTitle).toBeInTheDocument();
+    });
+
+    test("Verify that the tableHeaders prop works", async () => {
+        const name = screen.getByRole("columnheader", { name: /name/i });
+        expect(name).toBeInTheDocument();
+    });
+
+    test("Verify that the tableData prop works", async () => {
+        const firstName = screen.getByRole("cell", { name: /firstName/i });
+        expect(firstName).toBeInTheDocument();
+    });
 });
