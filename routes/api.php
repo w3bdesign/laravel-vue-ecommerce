955b8dbe72a0d560d779c6c5c02ea1fae0429aa2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-28 02:02:43 +0200

    Readd api and web routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+
+
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/product/{product}', [ProductController::class, 'show']);
+
+Route::post('/purchase', [UserController::class, 'purchase']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    //return ['Laravel' => app()->version()];
+    return view('welcome');
 });
 
 require __DIR__.'/auth.php';
